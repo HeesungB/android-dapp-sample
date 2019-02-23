26101225ca386451e77792df879b7840d19bf3de
HeesungB <hezo25@naver.com>
2019-02-23 12:25:36 +0900

    get wallet address

--- app/src/main/java/info/panicrooms/dapp_sample/MainActivity.java
+++ app/src/main/java/info/panicrooms/dapp_sample/MainActivity.java
@@ -1,13 +1,78 @@
 package info.panicrooms.dapp_sample;
 
+import android.databinding.DataBindingUtil;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+
+import info.panicrooms.dapp_sample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding mainBinding;
+
+    Web3j web3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainBinding.setMainLayout(this);
+
+        web3 = Web3j.build(new HttpService("http://172.30.1.16:8545"));
+
+        Web3AsyncTask asyncTask = new Web3AsyncTask();
+        asyncTask.execute();
+    }
+
+    public class Web3AsyncTask extends AsyncTask<String,Void,String>{
+
+        public String result;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().sendAsync().get();
+                String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+
+                Credentials credentials = Credentials.create("0x770a2e0421b189c656197de429aaf913a2dfd1bac293fa904258bd59e8fb03b6");
+                Log.d("zz",credentials.getAddress());
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+
+            return result;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
